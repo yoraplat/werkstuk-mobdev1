04b073c7409730a433a182a5fd9f7ccfff771dd0
yorapalt <yoraplat@student.arteveldehs.be>
2017-12-26 15:47:25 +0100

    css

--- app/assets/es/main.js
+++ app/assets/es/main.js
@@ -3,40 +3,44 @@
 
 import { Person, Student } from './models';
 import { GridOverlayElement } from './grid';
+import Navigation from './nav';
 
 class App {
-  constructor () {
-    console.log('Constructor of the class');
+  init () {}
 
-    document.registerElement('grid-overlay', GridOverlayElement);
-
-    this._gridOverlayElement = document.createElement('grid-overlay');
-    document.body.appendChild(this._gridOverlayElement);
-    this.resizeWindow();
-
-    window.addEventListener('resize', () => this.resizeWindow());
-  }
-
-  resizeWindow () {
-    this._gridOverlayElement.updateRendering(window.innerWidth, Math.max(
-      window.innerHeight,
-      document.body.offsetHeight,
-      document.documentElement.clientHeight
-    ), 24);
-  }
-
-  init () {
-    console.log('Initialization of the class App');
-
-    const ps1 = new Person('Philippe', 'De Pauw - Waterschoot');
-    console.log(ps1.toString());
-
-    const st1 = new Student('362453', 'philippe.depauw@arteveldehs.be', 'Philippe', 'De Pauw - Waterschoot');
-    console.log(st1.toString());
+  nav () {
+    let navClicks = 1;
+    let navState;
+    const hamburger = document.querySelector('.hamburger img');
+    const cross = document.querySelector('.cross');
+    const pageContent = document.querySelector('#container');
+    const nav = document.querySelector('#navigation');
+    console.log('test');
+    hamburger.addEventListener('click', ev => {
+        navClicks++;
+        if (navClicks % 2 == 0){
+          nav.style.display = 'block';
+          pageContent.style.display = 'none';
+        } else {
+          nav.style.display = 'none';
+          pageContent.style.display = 'block';
+        }
+    });
+    cross.addEventListener('click', ev => {
+      navClicks++;
+      if (navClicks % 2 == 0){
+        nav.style.display = 'block';
+        pageContent.style.display = 'none';
+      } else {
+        nav.style.display = 'none';
+        pageContent.style.display = 'block';
+      }
+  });
   }
 };
 
 window.addEventListener('load', (ev) => {
   const app = new App();
   app.init();
+  app.nav();
 });
